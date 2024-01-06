40d60db3abeda11809bcf36f7a4d4046ec93778b
douglasmariano <douglas.arm@live.com>
2024-01-05 21:15:30 -0300

    feat: Cadastro funcionando na api
    acertar o login

--- sistemaSkillWeb/src/context/SistemaSkillContext.jsx
+++ sistemaSkillWeb/src/context/SistemaSkillContext.jsx
@@ -6,6 +6,7 @@ export const SkillProvider = ({children}) => {
     const [nome, setNome] = useState('')
     const [login, setLogin] = useState('')
     const [senha, setSenha] = useState('')
+    const [email, setEmail] = useState("")
 
     const [skill, setSkill] = useState('')
     const [descricao, setDescricao] = useState('')
@@ -14,10 +15,11 @@ export const SkillProvider = ({children}) => {
     const [level, setLevel] = useState(0)
 
     const [usuarioLogado, setUsuarioLogado] = useState({})
+    const [mostrarSenha, setMostrarSenha] = useState(false);
 
     return(
         <SistemaSkillContext.Provider value={{
-            nome, setNome, login, setLogin, senha, setSenha, skill, setSkill, descricao, setDescricao, imagem, setImagem, level, setLevel, usuarioLogado, setUsuarioLogado
+            nome, setNome, login, setLogin, senha, setSenha, skill, setSkill, descricao, setDescricao, imagem, setImagem, level, setLevel, usuarioLogado, setUsuarioLogado, mostrarSenha, setMostrarSenha, email, setEmail
         }}>{children}</SistemaSkillContext.Provider>
     )
 }
--- sistemaSkillWeb/src/pages/cadastro/Cadastro.jsx
+++ sistemaSkillWeb/src/pages/cadastro/Cadastro.jsx
@@ -1,13 +1,80 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
+import { Eye, EyeSlash } from "react-bootstrap-icons";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { SistemaSkillContext } from "../../context/SistemaSkillContext";
+import { api } from "../../api/api";
 
 const Cadastro = () => {
+  const {
+    nome,
+    setNome,
+    senha,
+    setSenha,
+    mostrarSenha,
+    setMostrarSenha,
+    login,
+    setLogin,
+  } = useContext(SistemaSkillContext);
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const navigate = useNavigate();
+
+  const handleMostrarSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
+
+  const handleCadastrar = async (e) => {
+    e.preventDefault();
+
+    if (senha !== confirmarSenha) {
+      alert("A senha e a confirmação de senha não coincidem.");
+      return;
+    }
+
+    try {
+      const novoCadastro = {
+        nome: nome,
+        login: login,
+        senha: senha,
+      };
+
+      await api.post("/usuarios", novoCadastro);
+
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/");
+      setLogin("");
+      setSenha("");
+      setConfirmarSenha("");
+      setNome("");
+      getUsuarios();
+    } catch (error) {
+      console.error("Erro ao cadastrar usuário:", error);
+    }
+  };
+
+  const handleChangeNome = (e) => {
+    setNome(e.target.value);
+  };
+
+  const handleChangeLogin = (e) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangeSenha = (e) => {
+    setSenha(e.target.value);
+  };
+
+  const handleChangeConfirmarSenha = (e) => {
+    setConfirmarSenha(e.target.value);
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
       <Card border="dark" style={{ width: "35rem" }}>
@@ -23,8 +90,8 @@ const Cadastro = () => {
           Cadastro
         </Card.Header>
 
-        <Card.Body>
-          <Form>
+        <Card.Body style={{ alignItems: "center" }}>
+          <Form onSubmit={handleCadastrar}>
             <Form.Group
               as={Row}
               className="mb-3"
@@ -34,20 +101,30 @@ const Cadastro = () => {
                 Nome
               </Form.Label>
               <Col sm={12}>
-                <Form.Control type="nome" placeholder="Nome" />
+                <Form.Control
+                  required
+                  onChange={handleChangeNome}
+                  type="text"
+                  placeholder="Nome"
+                />
               </Col>
             </Form.Group>
 
             <Form.Group
               as={Row}
               className="mb-3"
-              controlId="formHorizontalEmail"
+              controlId="formHorizontalLogin"
             >
               <Form.Label column sm={12}>
                 Email
               </Form.Label>
               <Col sm={12}>
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control
+                  required
+                  onChange={handleChangeLogin}
+                  type="email"
+                  placeholder="Email"
+                />
               </Col>
             </Form.Group>
 
@@ -60,7 +137,23 @@ const Cadastro = () => {
                 Senha
               </Form.Label>
               <Col sm={12}>
-                <Form.Control type="senha" placeholder="Senha" />
+                <Form.Control
+                  required
+                  onChange={handleChangeSenha}
+                  type={mostrarSenha ? "text" : "password"}
+                  placeholder="Senha"
+                />
+                <Button
+                  variant="link"
+                  onClick={handleMostrarSenha}
+                  style={{
+                    position: "absolute",
+                    right: "10px",
+                    top: "55.5%",
+                  }}
+                >
+                  {mostrarSenha ? <EyeSlash /> : <Eye />}
+                </Button>
               </Col>
             </Form.Group>
 
@@ -74,28 +167,44 @@ const Cadastro = () => {
               </Form.Label>
               <Col sm={12}>
                 <Form.Control
-                  type="confirmarSenha"
+                  required
+                  onChange={handleChangeConfirmarSenha}
+                  type={mostrarSenha ? "text" : "password"}
                   placeholder="Confirmar Senha"
                 />
+                <Button
+                  variant="link"
+                  onClick={handleMostrarSenha}
+                  style={{
+                    position: "absolute",
+                    right: "10px",
+                    top: "72%",
+                  }}
+                >
+                  {mostrarSenha ? <EyeSlash /> : <Eye />}
+                </Button>
               </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3">
               <Col sm={{ span: 12, offset: 0 }} className="text-center">
-                <Button variant="dark" size="sm">
+                <Button variant="dark" size="sm" type="submit">
                   Cadastrar
                 </Button>
               </Col>
             </Form.Group>
           </Form>
         </Card.Body>
+
         <Card.Footer
           className="text-center"
           style={{ background: "transparent", borderTop: "0" }}
         >
           <Card.Text>
-            Já possui cadastro?
-            {/* <Link to="/login">Logar-se</Link> */}
+            Já possui cadastro?{" "}
+            <Link to="/login" style={{ color: "blue", marginLeft: "5px" }}>
+              Logar-se
+            </Link>
           </Card.Text>
         </Card.Footer>
       </Card>
